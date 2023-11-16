74550f1f56fc50d5651e675c8f0344210a68b71d
Aztaroz <74518192+Aztaroz@users.noreply.github.com>
2023-11-17 00:22:34 +0700

    Add files via upload

---
+++ Week_08/mqtt.py
@@ -0,0 +1,28 @@
+import paho.mqtt.client as mqtt
+host = "192.168.240.37"
+port = 1883
+
+import requests as req
+import json
+url = 'http://192.168.240.37:3000/data'
+
+def on_connect(self, client, userdata, rc):
+    print("MQTT Connected.")
+    self.subscribe("dht11")
+
+def on_message(client, userdata,msg):
+    global received_msg
+    
+    res = msg.payload.decode("utf-8")
+    received_msg = res
+    print(received_msg)
+
+    post = req.post(url, json=json.loads(res))
+    print(post)
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(host, port)
+client.loop_forever()
