31f455f716d538c1fba9067f70cf5392c953a2bc
Aztaroz <74518192+Aztaroz@users.noreply.github.com>
2023-11-17 00:20:00 +0700

    Add files via upload

---
+++ mqtt.ino
@@ -0,0 +1,175 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <uri/UriBraces.h>
+#include <uri/UriRegex.h>
+ESP8266WebServer server(80);
+bool led_status = false;
+
+const char* html = "<!DOCTYPE html>\n"
+                  "<html>\n"
+                  "  <script>\n"
+                  "    const led = async function(){\n"
+                  "      await fetch('/led')\n"
+                  "    }\n"
+                  "  </script>\n"
+                  "  <head>\n"
+                  "    <title>Home</title>\n"
+                  "  </head>\n"
+                  "  <body>\n"
+                  "    <center><h1>Welcome to ESP8266<h1></center>\n"
+                  "    <center><button id='bt' onclick='led()'>Toggle LED on/off LED</button></center>\n"
+                  "  </body>\n"
+                  "</html>";
+
+#include "DHT.h"
+DHT dht(D4, DHT11);
+float humid;
+float temp;
+
+#include <ArduinoJson.h>
+DynamicJsonDocument doc(50);
+
+// Update these with values suitable for your network.
+
+const char* ssid = "Aztaroz";
+const char* password = "14591459";
+const char* mqtt_server = "192.168.240.37";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '0') {
+    digitalWrite(D6, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else if ((char)payload[0] == '1') {
+    digitalWrite(D6, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      // client.publish("dht11", "hello world");
+      // ... and resubscribe
+      client.subscribe("dht11");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(D6, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  dht.begin();
+
+  server.on(F("/"), [](){      //V2
+    server.send(200, "text/html", html);
+  });
+
+  server.on(F("/led"), [](){
+    if (led_status){
+      client.publish("led", "1");
+      led_status = false;
+      server.send(200, "text/plain", "Off");
+    }
+    else {
+      client.publish("led", "0");
+      led_status = true;
+      server.send(200, "text/plain", "On");
+    }
+  });
+
+  server.begin();   //V2
+  Serial.println("HTTP server started");
+
+}
+
+void loop() {
+  server.handleClient();
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  humid = dht.readHumidity();
+  temp = dht.readTemperature();
+
+  
+  doc["temperature"] = round(temp);
+  doc["humidity"] = round(humid);
+  String json;
+  serializeJson(doc, json);
+
+  unsigned long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    ++value;
+    // snprintf (msg, MSG_BUFFER_SIZE, json.c_str());
+    Serial.print("Publish message: ");
+    Serial.println(json);
+    client.publish("dht11", json.c_str());
+
+    client.subscribe("led");
+  }
+}
